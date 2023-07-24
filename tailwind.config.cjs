7cd03c8f7a2c4945eafd2a9db6bb1c3d682df737
RyutoIkeuchi <ryu.i7133@gmail.com>
2023-07-24 23:59:29 +0900

    💄 ai-102の画像を追加

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'ai-102': "url('/public/assets/ai-102.jpg')",
+			}
+		}
 	},
 	plugins: [],
 }
